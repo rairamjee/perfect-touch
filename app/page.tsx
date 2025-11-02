ce3e17e582ecde1f2310946dd27c703a90cf4f66
Ramjee <ramjee@jmangroup.com>
2025-11-03 01:14:20 +0530

    feat: add slider page

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Slider from "@/assets/slider/page";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    
+    <div>
+      <Slider />
+    </div>
+  );
+}
---
+++ assets/slider/page.tsx
@@ -0,0 +1,9 @@
+export default function Slider() {
+  return (
+    <div>
+      <video width="100%" controls autoPlay loop muted>
+        <source src="/slider.mp4" type="video/mp4" />
+      </video>
+    </div>
+  );
+}
